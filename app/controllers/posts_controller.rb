b50fccc77cae180f3ca4c877ed8a587ccec84d3a
Bahati zirimwabagabo <bahatizirimwabagabo@gmail.com>
2023-11-10 22:12:37 +0200

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,15 +11,4 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
-  def create
-    @user = current_user
-    @post = @user.posts.new(post_params)
-
-    if @post.save
-      redirect_to user_posts_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
 end
